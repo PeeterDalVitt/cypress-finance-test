e308f506c3743dcdc2670506d50968012ea85b12
Peeter Dal Vitt <peeter1301@gmail.com>
2026-06-22 16:02:17 -0300

    Aplicação de LocalStorage e otimização da testagem

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -5,70 +5,47 @@
 context('Dev Finances', () => {
 
     beforeEach(() => {
-        cy.visit('https://devfinance-agilizei.netlify.app/#')
-        cy.get('#data-table tbody tr').should('have.length', 0)
+        cy.visit('https://devfinance.netlify.app/', {
+            onBeforeLoad: (win) => {
+                prepareLocalStorage(win)
+            }
+
+        })
+        
     });
     it('Cadastrar entradas', () => {
 
-        cy.get('#transaction .button').click()
+        cy.contains('#transaction .button', 'Nova Transação').click()
         cy.get('#description').type('Freelancer')
         cy.get('[name=amount]').type('500')
         cy.get('[type=date]').type('2024-06-01')
         cy.get('button').contains('Salvar').click()
     
-        cy.get('#data-table tbody tr').should('have.length', 1)
+        cy.get('#data-table tbody tr').should('have.length', 3)
     });
 
     it('Cadastrar saídas', () => {
         
-        cy.get('#transaction .button').click()
+        cy.contains('#transaction .button', 'Nova Transação').click()
         cy.get('#description').type('Aluguel')
         cy.get('[name=amount]').type('-800')
         cy.get('[type=date]').type('2024-06-01')
         cy.get('button').contains('Salvar').click()
 
-        cy.get('#data-table tbody tr').should('have.length', 1)
+        cy.get('#data-table tbody tr').should('have.length', 3)
 }); 
 
     it('Remover entradas e saídas', () => {
-        const entries = [
-            { description: 'Freelancer', amount: 500, date: '2024-06-01' },
-            { description: 'Aluguel', amount: -800, date: '2024-06-01' }
-        ];
+        
 
-        cy.get('#transaction .button').click()
-        cy.get('#description').type(entries[0].description)
-        cy.get('[name=amount]').type(entries[0].amount.toString())
-        cy.get('[type=date]').type(entries[0].date)
-        cy.get('button').contains('Salvar').click()
-
-        cy.get('#transaction .button').click()
-        cy.get('#description').type(entries[1].description)
-        cy.get('[name=amount]').type(entries[1].amount.toString())
-        cy.get('[type=date]').type(entries[1].date)
-        cy.get('button').contains('Salvar').click()
-
-        cy.get('td').contains(entries[0].description).parent().find('img[onclick*=remove]').click()
-        cy.get('td').contains(entries[1].description).parent().find('img[onclick*=remove]').click()
+        cy.get('td.description').contains("Mesada").parent().find('img[onclick*=remove]').click()
+        cy.get('td.description').contains('Suco Kapo').parent().find('img[onclick*=remove]').click()
         cy.get('#data-table tbody tr').should('have.length', 0)
 
 }); 
 
    it('Validar saldo com diversas transações', () => {
-  const entrada = 'Mesada'
-    const saida = 'KinderOvo'
-
-    cy.get('#transaction .button').click()
-    cy.get('#description').type(entrada)
-    cy.get('[name=amount]').type('100')
-    cy.get('[type=date]').type('2024-06-01')
-    cy.get('button').contains('Salvar').click()
-
-    cy.get('#transaction .button').click()
-    cy.get('#description').type(saida)
-    cy.get('[name=amount]').type('-35')
-    cy.get('[type=date]').type('2024-06-01')
-    cy.get('button').contains('Salvar').click()
+ 
 
   let incomes = 0
   let expenses = 0
